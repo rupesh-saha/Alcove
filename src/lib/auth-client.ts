06e61a4a45f44f709a22704c768f1b27093b78cd
rupesh-saha <saharupesh291@gmail.com>
2026-07-16 09:45:40 +0600

    fixed auth

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,7 @@
-import { createAuthClient } from "better-auth/react"
-export const authClient = createAuthClient({
+import { createAuthClient } from "better-auth/react";
 
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
-})
+export const authClient = createAuthClient({
+    baseURL: process.env.BETTER_AUTH_URL || "https://alcove-asthetics.vercel.app",
+});
 
 export const { signIn, signUp, useSession } = createAuthClient()
